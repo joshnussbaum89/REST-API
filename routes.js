b1425ec8a10bc79775785ac62b5dcb8ad10778d0
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-03-19 13:20:31 -0500

    add auth and reorganize middleware

--- routes.js
+++ routes.js
@@ -2,54 +2,12 @@
 
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const auth = require('basic-auth');
+const { asyncHandler } = require('./middleware/async-handler');
+const { authenticateUser } = require('./middleware/auth-user');
 const { User, Course } = require('./models');
 
 // Construct a router instance.
 const router = express.Router();
-
-// Handler function to wrap each route.
-function asyncHandler(cb) {
-    return async (req, res, next) => {
-        try {
-            await cb(req, res, next);
-        } catch (error) {
-            // Forward error to the global error handler
-            next(error);
-        }
-    }
-}
-
-const authenticateUser = async (req, res, next) => {
-    // Parse the user's credentials from the Authorization header.
-    let message;
-    const credentials = auth(req);
-    
-    if (credentials) {
-        const user = await User.findOne({ where: { emailAddress: credentials.name } });
-        // Store the user on the Request object.
-        req.currentUser = user;
-    }
-    //     // if (user) {
-    //     //     const authenticated = bcrypt
-    //     //         .compareSync(credentials.pass, user.confirmedPassword);
-    //     //     if (authenticated) {
-    //     //         console.log(`Authentication successful for username: ${user.username}`);
-
-    //     //         // Store the user on the Request object.
-    //     //         req.currentUser = user;
-    //     //     } else {
-    //     //         message = `Authentication failure for username: ${user.username}`;
-    //     //     }
-    //     // } else {
-    //     //     message = `User not found for username: ${credentials.name}`;
-    //     // }
-    // } else {
-    //     message = 'Auth header not found';
-    // }
-    console.log(credentials);
-    next();
-}
 
 // setup a friendly greeting for the root route
 router.get('/', (req, res) => {
@@ -58,11 +16,6 @@ router.get('/', (req, res) => {
     });
 });
 
-/**
- * TODO
- * THIS RETURNS ALL USERS
- * SHOULD ONLY RETURN THE AUTHENTICATED USER
- */
 // /api/users GET route that will return the currently authenticated user along with a 200 HTTP status code.
 router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
     const user = req.currentUser;
@@ -72,7 +25,7 @@ router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
 // /api/users POST route that will create a new user, set the Location header to "/", and return a 201 HTTP status code and no content.
 router.post('/users', asyncHandler(async (req, res) => {
     try {
-        // Use bcryptjs to has password
+        // Use bcryptjs to hash password
         const user = req.body;
         let { password } = user;
         user.password = bcrypt.hashSync(password, 10);
@@ -114,7 +67,7 @@ router.get('/courses/:id', asyncHandler(async (req, res) => {
 }));
 
 // /api/courses POST route that will create a new course, set the Location header to the URI for the newly created course, and return a 201 HTTP status code and no content.
-router.post('/courses', asyncHandler(async (req, res) => {
+router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
     try {
         await Course.create(req.body);
         res.status(201).location(`courses/${req.body.userId}`).json({ "message": "Course successfully created!" });
@@ -131,11 +84,11 @@ router.post('/courses', asyncHandler(async (req, res) => {
 }));
 
 // /api/courses/:id PUT route that will update the corresponding course and return a 204 HTTP status code and no content.
-router.put('/courses/:id', asyncHandler(async (req, res) => {
+router.put('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
     try {
         const course = await Course.findByPk(req.params.id);
         await course.update(req.body);
-        res.status(204).end();
+        res.json({ "message": "Course successfully edited" }).status(204).end();
     } catch (error) {
         console.log('ERROR: ', error.name);
 
@@ -149,10 +102,10 @@ router.put('/courses/:id', asyncHandler(async (req, res) => {
 }));
 
 // /api/courses/:id DELETE route that will delete the corresponding course and return a 204 HTTP status code and no content.
-router.delete('/courses/:id', asyncHandler(async (req, res) => {
+router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
     const course = await Course.findByPk(req.params.id);
     await course.destroy(req.body);
-    res.status(204).end();
+    res.json({ "message": "Course successfully deleted" }).status(204).end();
 }));
 
 
